b0a744fb2764a827da8a1958428dbede1d42f8da
Ronald Schonewille <RSchonewille@office.multisafepay.com>
2018-05-24 15:22:33 +0200

    PLGPRSS-323 Wrong version number indicated

--- multisafepay.php
+++ multisafepay.php
@@ -113,7 +113,7 @@ class Multisafepay extends PaymentModule
     {
         $this->name = 'multisafepay';
         $this->tab = 'payments_gateways';
-        $this->version = '4.1.0';
+        $this->version = '4.2.0';
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
         $this->author = 'MultiSafepay';
         $this->controllers = array('validation', 'payment');
