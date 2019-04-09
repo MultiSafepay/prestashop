7094b9fd06a67d23e1ae3f4132671c02bfd55b28
Jasper Nadi <jasper.nadi@multisafepay.com>
2019-04-09 12:44:46 +0200

    PLGPRSS-368: Bump version number to 4.3.0-rc2

--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -126,7 +126,7 @@ class MultiSafepayPaymentModuleFrontController extends ModuleFrontController
             "plugin" => array(
                 "shop" => 'Prestashop',
                 "shop_version" => _PS_VERSION_,
-                "plugin_version" => ' - Plugin 4.2.0',
+                "plugin_version" => ' - Plugin 4.3.0-rc2',
                 "partner" => "MultiSafepay",
             ),
         );
