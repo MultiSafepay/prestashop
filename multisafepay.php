60dbaf53702e5bc45a6be6cfdedc6307368400a9
Jasper-MultiSafepay <jasper.nadi@multisafepay.com>
2020-01-29 10:50:25 +0100

    Bump version to 4.5.1-RC1

--- multisafepay.php
+++ multisafepay.php
@@ -116,7 +116,7 @@ class Multisafepay extends PaymentModule
     {
         $this->name = 'multisafepay';
         $this->tab = 'payments_gateways';
-        $this->version = '4.5.0';
+        $this->version = '4.5.1-RC1';
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
         $this->author = 'MultiSafepay';
         $this->controllers = array('validation', 'payment');
